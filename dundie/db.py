e3204c1b7a1d33e04db42a50549afb32db8bf44d
Vitor O <vitorsantos.2018@alunos.utfpr.edu.br>
2024-03-21 10:44:17 -0300

    Injeção de dependencia da sessão

--- dundie/db.py
+++ dundie/db.py
@@ -1,9 +1,18 @@
 """Database connection"""
-from sqlmodel import create_engine
+from sqlmodel import Session, create_engine
 from .config import settings
+from fastapi import Depends
 
 engine = create_engine(
     settings.db.uri,  # pyright: ignore
     echo=settings.db.echo,  # pyright: ignore
     connect_args=settings.db.connect_args,  # pyright: ignore
 )
+
+
+def get_session():
+    with Session(engine) as session:
+        yield session
+
+
+ActiveSession = Depends(get_session)
