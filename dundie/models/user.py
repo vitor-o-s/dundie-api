893d04b6a19463c2b957d18a4cb08e9d3093fe5d
Vitor O <vitorsantos.2018@alunos.utfpr.edu.br>
2024-03-21 10:09:07 -0300

    Mapeamento de requisições

--- dundie/models/user.py
+++ dundie/models/user.py
@@ -2,6 +2,7 @@
 from typing import Optional
 from sqlmodel import Field, SQLModel
 from dundie.security import HashedPassword
+from pydantic import BaseModel, root_validator
 
 
 class User(SQLModel, table=True):
@@ -22,6 +23,38 @@ class User(SQLModel, table=True):
         """"Users belonging to management dept are admins."""
         return self.dept == "management"
 
+
 def generate_username(name: str) -> str:
     """Generates a slug username from a name"""
     return name.lower().replace(" ", "-")
+
+
+class UserResponse(BaseModel):
+    """Serializer for User Response"""
+
+    name: str
+    username: str
+    dept: str
+    avatar: Optional[str] = None
+    bio: Optional[str] = None
+    currency: str
+
+
+class UserRequest(BaseModel):
+    """Serializer for User request payload"""
+
+    name: str
+    email: str
+    dept: str
+    password: str
+    currency: str = "USD"
+    username: Optional[str] = None
+    avatar: Optional[str] = None
+    bio: Optional[str] = None
+
+    @root_validator(pre=True)
+    def generate_username_if_not_set(cls, values):
+        """Generates username if not set"""
+        if values.get("username") is None:
+            values["username"] = generate_username(values["name"])
+        return values
