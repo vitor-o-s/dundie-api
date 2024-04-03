ba32cc5087770245a2a49c23ac4e1a009b1e7e45
Vitor O <vitorsantos.2018@alunos.utfpr.edu.br>
2024-04-03 20:49:53 -0300

    Reset de senha com gestão de pedido do usuario

--- dundie/routes/user.py
+++ dundie/routes/user.py
@@ -1,6 +1,6 @@
 from typing import List
 
-from fastapi import APIRouter
+from fastapi import APIRouter, Body
 from fastapi.exceptions import HTTPException
 from sqlmodel import Session, select
 from sqlalchemy.exc import IntegrityError
@@ -14,6 +14,7 @@ from dundie.models.user import (
     UserPasswordPatchRequest
 )
 from dundie.auth import AuthenticatedUser, SuperUser, CanChangeUserPassword
+from dundie.tasks.user import try_to_send_pwd_reset_email
 
 router = APIRouter()
 
@@ -92,3 +93,12 @@ async def change_password(
     session.commit()
     session.refresh(user)
     return user
+
+# view
+@router.post("/pwd_reset_token/")
+async def send_password_reset_token(*, email: str = Body(embed=True)):
+    """Sends an email with the token to reset password."""
+    try_to_send_pwd_reset_email(email)
+    return {
+        "message": "If we found a user with that email, we sent a password reset token to it."
+    }
