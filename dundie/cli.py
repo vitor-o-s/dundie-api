50c6612880afa23fdd77143c0c292d4752620927
Vitor O <vitorsantos.2018@alunos.utfpr.edu.br>
2024-04-04 20:27:01 -0300

    Transaçoes via CLI

--- dundie/cli.py
+++ dundie/cli.py
@@ -4,6 +4,8 @@ from rich.table import Table
 from sqlmodel import Session, select
 
 from dundie.models.user import generate_username
+from dundie.tasks.transaction import add_transaction
+from dundie.models.transaction import Transaction, Balance
 from .config import settings
 from .db import engine
 from .models import User
@@ -20,6 +22,9 @@ def shell():
         "select": select,
         "session": Session(engine),
         "User": User,
+        "Transaction": Transaction,
+        "Balance": Balance,
+        "add_transaction": add_transaction,
     }
     typer.echo(f"Auto imports: {list(_vars.keys())}")
     try:
@@ -49,6 +54,7 @@ def user_list():
 
     Console().print(table)
 
+
 @main.command()
 def create_user(
     name: str,
@@ -73,3 +79,34 @@ def create_user(
         session.refresh(user)
         typer.echo(f"created {user.username} user")
         return user
+
+
+@main.command()
+def transaction(
+    username: str,
+    value: int,
+):
+    """Adds specified value to the user"""
+
+    table = Table(title="Transaction")
+    fields = ["user", "before", "after"]
+    for header in fields:
+        table.add_column(header, style="magenta")
+
+    with Session(engine) as session:
+        from_user = session.exec(select(User).where(User.username == "admin")).first()
+        if not from_user:
+            typer.echo("admin user not found")
+            exit(1)
+        user = session.exec(select(User).where(User.username == username)).first()
+        if not user:
+            typer.echo(f"user {username} not found")
+            exit(1)
+
+        from_user_before = from_user.balance
+        user_before = user.balance
+        add_transaction(user=user, from_user=from_user, session=session, value=value)
+        table.add_row(from_user.username, str(from_user_before), str(from_user.balance))
+        table.add_row(user.username, str(user_before), str(user.balance))
+
+        Console().print(table)
